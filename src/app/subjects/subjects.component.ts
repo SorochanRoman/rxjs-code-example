3f57bb97f36fd92cee6ce0d3a43e40aea0cfcfcc
Roman Sorochan <sorochan.roman@gmail.com>
2019-10-17 15:44:01 +0300

    subjects

--- src/app/subjects/subjects.component.ts
+++ src/app/subjects/subjects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, ReplaySubject, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-subjects',
@@ -10,6 +11,56 @@ export class SubjectsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // this.subject();
+    // this.replaySubject();
+    // this.behaviorSubject();
+  }
+
+  subject() {
+    const subject = new Subject();
+    subject.next(1);
+    const subscription1 = subject.subscribe(x => {
+      console.log('Subject 1:', x);
+    });
+    subject.next(2);
+    const subscription2 = subject.subscribe(x => {
+      console.log('Subject 2:', x);
+    });
+    subject.next(3);
+    subscription1.unsubscribe();
+    subject.next(4);
+  }
+
+  replaySubject() {
+    const subject = new ReplaySubject(2);
+    subject.next(1);
+    const subscription1 = subject.subscribe(x => {
+      console.log('ReplaySubject 1:', x);
+    });
+    subject.next(2);
+    subject.next(3);
+    subject.next(4);
+
+    const subscription2 = subject.subscribe(x => {
+      console.log('ReplaySubject 2:', x);
+    });
+    subscription1.unsubscribe();
+  }
+
+  behaviorSubject() {
+    const subject = new BehaviorSubject(2);
+    subject.next(1);
+    const subscription1 = subject.subscribe(x => {
+      console.log('behaviorSubject 1:', x);
+    });
+    subject.next(2);
+    subject.next(3);
+    subject.next(4);
+
+    const subscription2 = subject.subscribe(x => {
+      console.log('behaviorSubject 2:', x);
+    });
+    subscription1.unsubscribe();
   }
 
 }
