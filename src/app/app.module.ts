ecd20f2802525bef2a02f5f252a3958b4ecdb2e4
Roman Sorochan <sorochan.roman@gmail.com>
2019-10-17 15:02:55 +0300

    subjects init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { InitObservableComponent } from './init-observable/init-observable.component';
 import { OperatorsObservableComponent } from './operators-observable/operators-observable.component';
 import { HooOperatorsObservableComponent } from './hoo-operators-observable/hoo-operators-observable.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitObservableComponent,
     OperatorsObservableComponent,
-    HooOperatorsObservableComponent
+    HooOperatorsObservableComponent,
+    SubjectsComponent
   ],
   imports: [
     BrowserModule
