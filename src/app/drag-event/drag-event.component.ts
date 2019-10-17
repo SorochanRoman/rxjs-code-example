8c6a745a7bb7d7297b6de46c9ce66d03cb15447b
Roman Sorochan <sorochan.roman@gmail.com>
2019-10-17 17:43:41 +0300

    drag event

--- src/app/drag-event/drag-event.component.ts
+++ src/app/drag-event/drag-event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { switchMap, takeUntil } from 'rxjs/operators';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { fromEvent, } from 'rxjs';
 
 @Component({
   selector: 'app-drag-event',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragEventComponent implements OnInit {
 
+  @ViewChild('drag', { static: true }) drag: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const dragMouseDownEvents$ = fromEvent(this.drag.nativeElement, 'mousedown');
+    const documentMouseMoveEvents$ = fromEvent(document, 'mousemove');
+    const documentMouseUpEvents$ = fromEvent(document, 'mouseup');
+
+    dragMouseDownEvents$.pipe(
+      switchMap(() => documentMouseMoveEvents$.pipe(
+        takeUntil(documentMouseUpEvents$)
+      ))
+    ).subscribe((e: any) => {
+      console.log(e);
+    });
+
   }
 
 }
